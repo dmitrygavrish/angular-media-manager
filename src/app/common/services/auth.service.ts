1aee886f643b02dd575aefb7f8d4b6c1c0f0131d
dmitrygavrish <dmitrygavrish@gmail.com>
2017-06-06 21:02:14 +0300

    minor update

--- src/app/common/services/auth.service.ts
+++ src/app/common/services/auth.service.ts
@@ -7,30 +7,30 @@ import {LocalService} from './local.service';
 export class AuthService {
   private _authState: AuthState = {isLoggedIn: true};
   private _authState$$: Subject<AuthState> = new Subject();
-  
+
   public constructor(private _mainService: LocalService) {
   }
-  
+
   public validateLogin(): void {
     this._mainService.processLogin().subscribe((isValid: boolean) => {
-    
+      this.updateAuth({isLoggedIn: isValid});
     });
   }
-  
+
   public updateAuth(authState: AuthState): void {
     // allow passing incomplete AuthState objects by rewriting only those
     // stored _authState properties that were passed in authState argument
     for (const key in authState) {
       this._authState[key] = authState[key];
     }
-    
+
     this._authState$$.next(authState);
   }
-  
+
   public get isLoggedIn(): boolean {
     return this._authState.isLoggedIn;
   }
-  
+
   public get authState$(): Observable<AuthState> {
     return this._authState$$.asObservable();
   }
--- src/app/common/services/local.service.ts
+++ src/app/common/services/local.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http} from "@angular/http";
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class LocalService {
+  public constructor(_http: Http) {
+  }
 
-  constructor() { }
+  public processLogin(): Observable<boolean> {
+    // TODO implement localStorage
+    return Observable.of(true);
+  }
 
 }
